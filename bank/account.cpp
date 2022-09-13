d30794c2740ef65c0715766f5a4d47d7cbc35d70
Lxll <2473475362@qq.com>
2022-09-13 16:51:51 +0800

    完成

--- bank/account.cpp
+++ bank/account.cpp
@@ -5,11 +5,14 @@
 #include <iomanip>
 #include <sstream>
 using namespace std;
+
 double Account::total = 0;
 Account::Account(Date date, string id)
 {
 	this->id = id;
 	this->balance = 0.0f;
+
+	cout << setw(16) << setiosflags(ios::left) << date.show() <<"#" << this->getId() << " created" << endl;
 }
 // 存入款实现
 void Account::record(Date date, double amount, string desc) 
@@ -23,11 +26,9 @@ void Account::record(Date date, double amount, string desc)
 		<< setw(8) << setiosflags(ios::left) << this->getBalance() << desc << endl;
 }
 // 展示
-string Account::show() const
+void Account::show() const
 {
-	stringstream ss;
-	ss <<"#" << setw(15) << this->id << "Balance: " << this->getBalance();
-	return ss.str();
+	cout<< setw(16) << this->id << "Balance: " << this->getBalance();
 }
 // 报错
 void Account::error(string msg)
@@ -48,7 +49,7 @@ double Account::getTotal()
 }
 
 
-double SavingsAccount::total = 0;
+
 SavingsAccount::SavingsAccount(Date date, string id, double rate):Account(date,id),acc(date,0.0f)
 {
 	this->rate = rate;
@@ -72,13 +73,104 @@ void SavingsAccount::withdraw(Date date, double amount, string desc)
 }
 void SavingsAccount::settle(Date date)
 {
-	
-	double amount = acc.getSum();
-	amount = floor(amount * 100 + 0.5) / 100;
-	record(date, amount,"interest");
+	Date temp(date.getYear() - 1, 1, 1);
+	acc.change(date, getBalance());
+	double amount = acc.getSum() * rate / temp.getMaxDays();
 	acc.reset(date, this->getBalance());
+	amount = floor(amount * 100 + 0.5) / 100;
+	deposit(date, amount,"interest");
+	
 }
 double SavingsAccount::getRate() const
 {
 	return this->rate;
+}
+
+
+
+CreditAccount::CreditAccount(Date date, string id, double credit, double rate, double fee):Account(date,id),acc(date,0) 
+{
+	this->credit = credit;
+	this->rate = rate;
+	this->fee = fee;
+}
+// 存款
+void CreditAccount::deposit(Date date, double amount, string desc)
+{
+	record(date, amount, desc);
+	if (getBalance() < 0)
+	{
+		acc.change(date, getBalance());
+	}
+	else {
+		acc.change(date, 0);
+	}
+}
+// 取钱
+void CreditAccount::withdraw(Date date, double amount, string desc)
+{
+	if (getBalance() - amount < -getCredit())
+	{
+		error("not credit");
+	}
+	else {
+		record(date, -amount,desc);
+		if (getBalance() < 0)
+		{
+			acc.change(date, getBalance());
+		}
+		else {
+			acc.change(date, 0);
+		}
+	}
+}
+// 结算
+void CreditAccount::settle(Date date)
+{
+	// 结算利息
+	acc.change(date, getBalance());
+	double amount = -acc.getSum() * rate;
+	amount = floor(amount * 100 + 0.5) / 100;
+	acc.reset(date, this->getBalance());
+	if (amount > 1e-5) {
+		withdraw(date, amount, "interest");
+	}
+
+	// 年费支出
+	if (date.getMonth() == 1 && date.getDay() == 1)
+	{
+		withdraw(date, fee, "annual fee");
+	}
+
+}
+
+void CreditAccount::show() const {
+	cout << setw(16) << setiosflags(ios::left) << this->getId()
+		<< "Balance: " << setw(7) << this->getBalance()
+		<< "Available credit:" << this->getAvailable();
+}
+
+double CreditAccount::getCredit() const
+{
+	return credit;
+}
+double CreditAccount::getRate() const
+{
+	return rate;
+}
+double CreditAccount::getFee() const
+{
+	return fee;
+}
+// 剩余信用额度
+double CreditAccount::getAvailable() const
+{
+	if (getBalance() < 0)
+	{
+		return credit + getBalance();
+	}
+	else {
+		return credit;
+	}
+	
 }
